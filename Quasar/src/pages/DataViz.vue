36be4fa065517aa3d89876020d6d4a5170b1be17
Qiao Zhang <zqthedesigner@gmail.com>
2021-12-12 01:07:47 +0800

    update

--- Quasar/src/pages/DataViz.vue
+++ Quasar/src/pages/DataViz.vue
@@ -1,7 +1,11 @@
 <template>
   <q-page class="items-center justify-evenly">
-    <h1>Data Viz</h1>
+    <!-- <h1>Data Viz</h1> -->
     <GoogleCharts />
+
+    <div class="bg-green-4 q-ma-lg absolute fit ">
+      <h3 class="absolute-center">Some Title</h3>
+    </div>
   </q-page>
 </template>
 
--- Quasar/src/pages/ExtendedStyle.vue
+++ Quasar/src/pages/ExtendedStyle.vue
@@ -12,7 +12,8 @@
             <p>
               Use
               <code class="bg-grey-2 q-px-xs">q-border-{size variable} </code>
-              for different thickness of borders.
+              for different thickness of borders. Border thickness ratio is
+              following fibonacci sequence.
             </p>
           </div>
 
@@ -92,6 +93,63 @@
           </div>
         </div>
       </div>
+
+      <div class="q-horizontal-divider full-width q-my-lg">
+        <div style="max-width: 700px">
+          <b>Divider:</b> use <code>q-{horizontal / vertical}-divider</code> for
+          horizontal or vertical dividers. Use together
+          <code>q-border-{size}</code> and <code>q-border-{color}</code> to
+          change the thickness and color of the divider
+        </div>
+      </div>
+
+      <div class="row q-col-gutter-md">
+        <!-- Background Opacity -->
+        <div class="col-3">
+          <div class="scoped-style-description">
+            <p><b>Background Color Opacity</b></p>
+            <p>
+              Use
+              <code class="bg-grey-2 q-px-xs"
+                >q-bg-{color variable}-alpha-{1 - 10}
+              </code>
+              for different background color transprancy.
+            </p>
+          </div>
+          <div class="q-gutter-sm bg-green-alpha-2 q-pr-sm q-pt-xs q-pb-sm">
+            <div
+              v-for="i in 5"
+              :key="i"
+              :class="`bg-sentient-purple-alpha-${i * 2}`"
+            >
+              bg-sentient-purple-alpha-{{ i * 2 }}
+            </div>
+          </div>
+        </div>
+
+        <!-- Shadow Color -->
+        <div class="col-3">
+          <div class="scoped-style-description">
+            <p><b>Shadow Color</b></p>
+            <p>
+              Use
+              <code class="bg-grey-2 q-px-xs"
+                >q-shadow-{size variable}-{color variable}
+              </code>
+              for different background color transprancy.
+            </p>
+          </div>
+          <div class="q-gutter-md q-pr-sm q-pt-xs q-pb-sm">
+            <div
+              v-for="color in colors"
+              :key="color"
+              :class="`shadow-sm-${color}`"
+            >
+              shadow-sm-${color}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="row q-gutter-sm">
       <div class="col-2 q-gutter-sm">
@@ -115,6 +173,11 @@
         <h5 class="text-red-alpha-4">Text Red Alpha 4</h5>
         <h5 class="text-red-alpha-6">Text Red Alpha 6</h5>
         <h5 class="text-red-alpha-8">Text Red Alpha 8</h5>
+      </div>
+
+      <div>
+        <q-img src="~/assets/Car.png" alt="" class="recolor" />
+        <q-img src="~/assets/Van.png" alt="" />
       </div>
     </div>
   </q-page>
@@ -146,4 +209,5 @@ export default defineComponent({
 .scoped-style-description {
   height: 180px;
 }
+
 </style>
